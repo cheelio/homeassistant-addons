c2b197eebbcce7f52acb49f79f6a9a7877a01e8d
cheelio <cheelio@gmail.com>
2025-07-11 18:31:37 +0200

    bump

--- immich-public-proxy-addon/run.sh
+++ immich-public-proxy-addon/run.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/with-contenv bashio
 
-echo "IMMICH_URL=$IMMICH_URL"
+IMMICH_URL=$(bashio::config 'immich_url')
+
+export IMMICH_URL
+export API_KEY
+
+echo "IMMICH_URL is: $IMMICH_URL"
 node dist/index.js
